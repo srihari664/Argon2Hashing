7366cdfe6d8acfe60ae37837c111df6082c5061d
Uppala Srihari <harisri664@gmail.com>
2025-03-05 23:55:01 +0530

    Changing the password input dynamically

--- src/main/java/com/srihari/argon2main/Argon2HashingApplication.java
+++ src/main/java/com/srihari/argon2main/Argon2HashingApplication.java
@@ -1,15 +1,19 @@
 package com.srihari.argon2main;
 
+import java.util.Scanner;
+
 import com.srihari.argon2hash.Argon2PasswordHasher;
 
 public class Argon2HashingApplication {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		Argon2PasswordHasher hasher = new Argon2PasswordHasher();
 
-		// Example password
-		String password = "MySecurePassword123";
-
+		// Get user input for password
+        System.out.print("Enter your password: ");
+        String password = scanner.nextLine();
+        
 		// Hash the password
 		String hashedPassword = hasher.hashPassword(password);
 		System.out.println("Hashed password: " + hashedPassword);
@@ -21,6 +25,7 @@ public class Argon2HashingApplication {
 		// Verify with wrong password
 		boolean isInvalid = hasher.verifyPassword(hashedPassword, "WrongPassword");
 		System.out.println("Wrong password verification result: " + isInvalid);
+		scanner.close();
 	}
 
 }
